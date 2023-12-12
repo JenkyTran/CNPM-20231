83748378be607ed51be9c311f7f8d3fb4bffd008
Jenky Trần <tranvanluc.work@gmail.com>
2023-12-12 14:57:31 +0700

    Initial commit

---
+++ src/main/java/com/example/cnpm20231/Cnpm20231Application.java
@@ -0,0 +1,13 @@
+package com.example.cnpm20231;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cnpm20231Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cnpm20231Application.class, args);
+    }
+
+}
